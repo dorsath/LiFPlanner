5e9592dc499496bed4834214d09c171b3fa0e452
Ronald <selsery@gmail.com>
2014-11-01 18:04:55 +0100

    Some more style and a bit of text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user!
+
   after_filter :set_csrf_cookie_for_ng
 
   def set_csrf_cookie_for_ng
