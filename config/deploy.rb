d0b057716d97e678b10168bfc9ba41d9b486d1ef
Ronald <selsery@gmail.com>
2014-11-02 19:13:17 +0100

    Angular issue fixed. Now onto the assets precompile. take 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,12 +57,3 @@ namespace :deploy do
   end
 
 end
-
-before "deploy:symlink", "assets:precompile"
-
-namespace :assets do
-  desc "Compile assets"
-  task :precompile, :roles => :app do
-    run "cd #{release_path} && rake RAILS_ENV=#{rails_env} assets:precompile"
-  end
-end
